1f8f591d45d81ddd052cd56b7b27a7f2ae24e900
wkaewnang <w.kaewnang@gmail.com>
2015-03-23 16:28:45 +0100

    Modify waiter brain

--- src/com/insta/iwaiter/AI/WaiterBrain.java
+++ src/com/insta/iwaiter/AI/WaiterBrain.java
@@ -31,15 +31,23 @@ public class WaiterBrain implements BrainService {
 			simulator.moveDown();
 		} else if (step >= 65 && step < 80) {
 			simulator.moveRight();
-		} else if (step >= 80 && step < 143) {
+		} else if (step >= 80 && step < 112) {
 			simulator.moveUp();
-		} else if (step >= 143 && step < 157) {
+		} else if (step >= 112 && step < 175) {
+			simulator.moveRight();
+		} else if (step >= 175 && step < 195) {
+			simulator.moveUp();
+		} else if (step >= 195 && step < 255) {
 			simulator.moveLeft();
-		} else if (step >= 157 && step < 188) {
+		} else if (step >= 255 && step < 265) {
+			simulator.moveUp();
+		} else if (step >= 265 && step < 280) {
+			simulator.moveLeft();
+		} else if (step >= 280 && step < 310) {
 			simulator.moveDown();
-		} else if (step >= 188 && step < 220) {
+		} else if (step >= 310 && step < 345) {
 			simulator.moveLeft();
-		} else if (step >= 220) {
+		} else if (step >= 340) {
 			simulator.stop();
 		}
 	}
